b92af84b6fc86b1c9b4c65fea00e70af7527b9db
kimyoonje <forteas2003@hanmail.net>
2021-11-14 22:06:05 +0900

    [김윤제] 과제물 3~4주차

---
+++ src/org/sparta/hellgorithm/week0304/homework/yjk/test.java
@@ -0,0 +1,7 @@
+package org.sparta.hellgorithm.week0304.homework.yjk;
+
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
--- src/org/sparta/hellgorithm/week0304/lesson/Factorial.java
+++ src/org/sparta/hellgorithm/week0304/lesson/Factorial.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Factorial {
     static int factorial(int n) {
         if ( n > 0 ) {
-            return n * factorial(n-1)
+            return n * factorial(n-1);
         }
         else {
             return 1;
