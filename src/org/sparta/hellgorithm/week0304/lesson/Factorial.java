0b2d80c6d93abefc70d0e6e5ebec859e4d8bb1e6
kimyoonje <forteas2003@hanmail.net>
2021-11-14 22:02:34 +0900

    [김윤제] 3~4주차 과제 재귀알고리즘

---
+++ src/org/sparta/hellgorithm/week0304/lesson/Factorial.java
@@ -0,0 +1,27 @@
+package org.sparta.hellgorithm.week0304.lesson;
+
+import java.util.Scanner;
+
+/*
+    팩토리얼을 재귀적으로 구현
+ */
+public class Factorial {
+    static int factorial(int n) {
+        if ( n > 0 ) {
+            return n * factorial(n-1)
+        }
+        else {
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("정수를 입력하세요 : ");
+
+        int x = stdIn.nextInt();
+
+        System.out.println(x +"의 팩토리얼은 " + factorial(x)+ "입니다.");
+    }
+}
